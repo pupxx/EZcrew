eb4c748eaf8dd228e5833c6dcc0afd0870a9eca0
Cheryln <cherylnbarber@gmail.com>
2017-07-10 20:55:32 -0700

    Edit link has been formated on the page and resized.

--- server/models/APIpracticesModel.js
+++ server/models/APIpracticesModel.js
@@ -7,7 +7,7 @@ class Practices {
   static getAllPractices(){
     // return knex('practices').orderBy('date', 'DESC')
     return knex('*')
-    .from('users_practices').select('users_practices.id as userPractice.id', 'practices.id as practices.id', 'users_practices.attending', 'practices.practiceDescription', 'practices.date').innerJoin('practices', 'practices.id', 'users_practices.id').where('users_practices.user_id', 1).orderBy('date', 'DESC')
+    .from('users_practices').select('users_practices.id as userPractice.id', 'practices.id as practices.id', 'users_practices.attending', 'practices.practiceDescription', 'practices.date').innerJoin('practices', 'practices.id', 'users_practices.id').where('users_practices.user_id', 1).orderBy('date', 'ASC')
   }
 
 }
