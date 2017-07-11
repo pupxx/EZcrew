90b2ab7b65742bfb415464370af4a7a9d69d187e
Cheryln <cherylnbarber@gmail.com>
2017-07-10 21:44:02 -0700

    Cleaned up the formatting of getAllPractices route in the APIpracticesModel.js page.

--- server/models/APIpracticesModel.js
+++ server/models/APIpracticesModel.js
@@ -8,7 +8,10 @@ class Practices {
     let today = new Date()
     // return knex('practices').orderBy('date', 'DESC')
     return knex('users_practices')
-    .select('users_practices.id as userPractice.id', 'practices.id as practices.id', 'users_practices.attending', 'practices.practiceDescription', 'practices.date').innerJoin('practices', 'practices.id', 'users_practices.id').where('users_practices.user_id', 1).andWhere('practices.date', '>=', 'today')
+    .select('users_practices.id as userPractice.id', 'practices.id as practices.id', 'users_practices.attending', 'practices.practiceDescription', 'practices.date')
+    .innerJoin('practices', 'practices.id', 'users_practices.id')
+    .where('users_practices.user_id', 1)
+    .andWhere('practices.date', '>=', 'today')
   }
 
 }
