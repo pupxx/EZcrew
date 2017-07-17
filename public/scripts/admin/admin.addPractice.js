8b09b251ecb4f01d8100fc0b541be036bc2be3ce
Cheryln <cherylnbarber@gmail.com>
2017-07-16 19:37:04 -0700

    Refactored the addPractice function.

--- public/scripts/admin/admin.addPractice.js
+++ public/scripts/admin/admin.addPractice.js
@@ -13,6 +13,7 @@
     vm.$onInit = onInit;
     vm.toggleForm = toggleForm;
     vm.addPractice = addPractice;
+    vm.buildArrayToPost = buildArrayToPost;
 
     function onInit (){
       vm.show = false
@@ -26,40 +27,36 @@
       }
     }
 
-    // function addPractice(){
-    //   let body = vm.practice;
-    //   adminService.addPractice(body)
-    // }
+    // this function will add a practice to the practices table and then it will post that practice to the users_practices table adding the practice once for each user so we can manage attendance. This allows the user to edit an already existing practice when they check the box for attendance.
 
-
-    // this function will add a practice to the practices table and then it will post that practice with each of the users_practices table adding the practice once for each user so we can manage attendance.
-    // This allows the user to edit an already existing practice when they check the box for attendance.
     function addPractice(){
       let body = vm.practice;
       delete vm.practice;
       adminService.addPractice(body).then(()=>{
         userService.getAllUsers().then(()=>{
           vm.allUsers = userService.allUsers;
-          vm.addedPractice = adminService.addedPractice;
-
-          let list = []
-          vm.allUsers.forEach((el)=>{
-            let userPractice = {
-              user_id: el.id,
-              practice_id: vm.addedPractice.id,
-              attending: false
-            }
-            list.push(userPractice)
-          })
-          return list
+          return vm.buildArrayToPost(vm.allUsers);
         })
         .then((arrayToPost)=>{
-          let list = {
-            arrayToPost
-          }
+          let list = { arrayToPost }
           adminService.addPracticeForAllUsers(list);
-        })
+        });
+      });
+    }
+
+    //this builds and array to post to the users_practices table.
+    function buildArrayToPost(array){
+      vm.addedPractice = adminService.addedPractice;
+      let list = []
+      array.forEach((el)=>{
+        let userPractice = {
+          user_id: el.id,
+          practice_id: vm.addedPractice.id,
+          attending: false
+        }
+        list.push(userPractice);
       })
+      return list
     }
 
 
