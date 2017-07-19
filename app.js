22ccee30305c2abc430f479002326af741001675
Cheryln <cherylnbarber@gmail.com>
2017-07-19 13:11:53 -0700

    Changed .constant in app.module and then changed it back again.

--- app.js
+++ app.js
@@ -12,11 +12,7 @@ var admin = require('./server/routes/adminRoutes');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
