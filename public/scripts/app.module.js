a6ee44c616d425848128e6f79f9349ccd4e0405e
Cheryln <cherylnbarber@gmail.com>
2017-07-19 17:24:19 -0700

    switched to dev environment.

--- public/scripts/app.module.js
+++ public/scripts/app.module.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict'
   angular.module('app', [ 'ui.router', 'angularMoment'])
-  .constant('API_BASE_URL', 'https://ez-crew.herokuapp.com')
-  // .constant('API_BASE_URL', 'http://localhost:3000')
+  // .constant('API_BASE_URL', 'https://ez-crew.herokuapp.com/')
+  .constant('API_BASE_URL', 'http://localhost:3000')
 })()
